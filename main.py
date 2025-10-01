3f06afda1416ec0286fd058fa38d6cc9e41aba81
StevenXuf <chasefreedom@163.com>
2025-10-01 17:15:31 +0200

    modify loader

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ import torch
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
 
-from utils import get_default_config
+from utils import get_default_config, get_image_transform
 
 class ArchiveDataset(Dataset):
     def __init__(self, image_folder, anno_folder):
         # Load all CSV files in the folder
         self.image_folder = image_folder
         self.anno_folder = anno_folder
-        all_files = glob.glob(os.path.join(folder_path, "*.csv"))
+        all_files = glob.glob(os.path.join(self.anno_folder, "*.csv"))
         dataframes = []
 
         for file in all_files:
@@ -23,7 +23,8 @@ class ArchiveDataset(Dataset):
             df = df.dropna(subset=['photo_id', 'description'])
             df = df[(df['photo_id'].astype(str).str.strip() != '') & 
                     (df['description'].astype(str).str.strip() != '')]
-
+            df['photo_path'] = df["photo_id"].apply(lambda x: os.path.join(self.image_folder, x[:2], x[2:4], f'{x}.jpg'))
+            df = df[df["photo_path"].apply(os.path.exists)].reset_index(drop=True)
             dataframes.append(df)
 
         # Combine all CSVs into one DataFrame
@@ -35,14 +36,14 @@ class ArchiveDataset(Dataset):
     def __getitem__(self, idx):
         row = self.data.iloc[idx]
         image_id = row['photo_id']
-        image_folder = os.path.join(self.image_folder, image_id[:2], image_id[2:4], f'{image_id}'.jpg)
-        pil_image = Image.open(image_folder).convert('RGB').resize((224, 224), Image.Resampling.BICUBIC)
+        image_path = row['photo_path']
+        pil_image = Image.open(image_path).convert('RGB').resize((224, 224), Image.Resampling.BICUBIC)
         description = row['description']
 
-        # Return as dictionary (can be customized for your use case)
         return {
             'image': pil_image,
             'image_id': image_id,
+            'image_path': image_path,
             'caption': description
         }
     
@@ -70,10 +71,13 @@ if __name__ == "__main__":
     image_folder = cfg['IMAGE_FOLDER']
     anno_folder = cfg['ANNOTATION_FOLDER']
     batch_size = cfg['BATCH_SIZE']
-    data_loader = get_archive_loader(image_folder=image_folder, anno_folder=anno_folder, batch_size=batch_size)
+    image_size = cfg['IMAGE_SIZE']
+    mean = cfg['MEAN']
+    std = cfg['STD']
+    img_transform = get_image_transform(image_size, mean, std)
+    data_loader = get_archive_loader(image_folder=image_folder, anno_folder=anno_folder, batch_size=batch_size, transform=img_transform)
 
     for batch in data_loader:
         image = batch['image']
         print(image.size())
-        print
         break
--- utils.py
+++ utils.py
@@ -1,7 +1,19 @@
 import yaml
 
+from torchvision import transforms
+
 def get_default_config(config_path='./config.yaml'):
     with open(config_path,'r') as f:
         return yaml.safe_load(f)
+
+def get_image_transform(image_size, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
+    transform = transforms.Compose([
+        transforms.Resize((image_size, image_size)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=mean,
+                            std=std)
+    ])
+
+    return transform
     
 
